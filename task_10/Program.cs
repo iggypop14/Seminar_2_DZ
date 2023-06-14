c32ba1863f5230ebfab8271129c61c47b1453500
Igor <test@mail.ru>
2023-06-14 09:58:47 +0300

    Добавил решение task10

---
+++ task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+System.Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(num/10);
+//System.Console.WriteLine(num1); // Для контроля вывода
+int num2 = Convert.ToInt32(num1%10);
+System.Console.WriteLine(num2); // Вывод второй цифры
